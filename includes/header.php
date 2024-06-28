1120fa8d6e377963c79d8794f01316eb9d69f6db
Emile Greyling <109597128+EmileGreyling@users.noreply.github.com>
2024-06-28 19:39:35 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -9,9 +9,18 @@
 </head>
 
 <body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid justify-content-center">
-            <span class="navbar-brand mb-0 h1">PHP TASKS</span>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand">PHP TODO</a>
+            <div class="ml-auto d-flex gap-2">
+                <?php if (isset($_SESSION['user_id'])) : ?>
+                    <a class="btn btn-danger" href="logout.php">Logout</a>
+                <?php else : ?>
+                    <a class="btn btn-primary" href="login.php">Login</a>
+                    <a class="btn btn-success" href="register.php">Register</a>
+                <?php endif; ?>
+            </div>
+        </div>
         </div>
     </nav>
     <div class="container mt-4">
