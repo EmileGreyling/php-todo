c7f491063049fba70bdfd374491ffb6e2a28a032
Emile Greyling <109597128+EmileGreyling@users.noreply.github.com>
2024-06-28 08:10:37 +0200

    Render tasks in table

--- index.php
+++ index.php
@@ -21,14 +21,14 @@
                     <input type="text" class="form-control" name="task" placeholder="What do you want to do?">
                 </div>
                 <div class="col-sm-2">
-                    <button type="submit" class="btn btn-primary px-5">Add Task</button>
+                    <button type="submit" class="btn btn-primary px-5" name="add">Add Task</button>
                 </div>
             </div>
         </form>
 
         <table class="table">
             <thead>
-                <tr>
+                <tr class="text-center">
                     <th scope="col">#</th>
                     <th scope="col">Tasks</th>
                     <th scope="col">Status</th>
@@ -36,6 +36,28 @@
                 </tr>
             </thead>
             <tbody>
+                <?php
+                require 'config.php';
+
+                $tasks = $db->query("SELECT * FROM task");
+                ?>
+
+                <?php foreach ($tasks as $row) : array_map('htmlentities', $row); ?>
+                    <tr class="text-center">
+                        <th scope="row"><?php echo implode('</td><td>', $row); ?></th>
+                        <td colspan="2" class="action">
+                            <?php
+                            if ($row['status'] != "Done") {
+                                echo
+                                '<a href="update_task.php?task_id=' . $row['task_id'] . '"class="btn p-0">✅</a>';
+                            }
+                            ?>
+                            <a href="delete_task.php?task_id=<?php echo $row['task_id'] ?>" class="btn p-0">
+                                ❌
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
