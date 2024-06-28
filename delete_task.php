9b4b2164104a1608e863cff0c784c63df3d09784
Emile Greyling <109597128+EmileGreyling@users.noreply.github.com>
2024-06-28 08:18:29 +0200

    Create delete_task.php

---
+++ delete_task.php
@@ -0,0 +1,15 @@
+<?php
+require "config.php";
+
+if ($_GET['task_id']) {
+    $task_id = $_GET['task_id'];
+
+    $deletingtasks = mysqli_query(
+        $db,
+        "DELETE FROM task WHERE task_id=$task_id"
+    )
+        or
+        die(mysqli_error($db));
+
+    header("location: index.php");
+}
