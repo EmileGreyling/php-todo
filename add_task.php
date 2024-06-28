af7ce0db031238cdb62e452ca74bd1a38731071f
Emile Greyling <109597128+EmileGreyling@users.noreply.github.com>
2024-06-28 08:24:50 +0200

    Refactor code to add a task

--- add_task.php
+++ add_task.php
@@ -5,11 +5,12 @@ if (isset($_POST["add"])) {
     if ($_POST["task"] != "") {
         $task = $_POST["task"];
 
-        if ($db->query("INSERT INTO task (task, status) VALUES('$task', 'Pending')") === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $db->error;
-        }
+        $addtasks = mysqli_query(
+            $db,
+            "INSERT INTO task (task, status) VALUES('$task', 'Pending')"
+        )
+            or
+            die(mysqli_error($db));
         header('location:index.php');
     }
 }
