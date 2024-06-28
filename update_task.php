d6f3d469bdcf15d49baa44dd49e11d60b0bee5cf
Emile Greyling <109597128+EmileGreyling@users.noreply.github.com>
2024-06-28 08:25:21 +0200

    Create update_task.php

---
+++ update_task.php
@@ -0,0 +1,15 @@
+<?php
+require "config.php";
+
+if ($_GET['task_id']) {
+    $task_id = $_GET['task_id'];
+
+    $updatingtasks =
+        mysqli_query(
+            $db,
+            "UPDATE task SET status='Done' WHERE task_id=$task_id"
+        )
+        or
+        die(mysqli_error($db));
+    header('location: index.php');
+}
